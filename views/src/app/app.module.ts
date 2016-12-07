93e6e3ff30064a975febd32ef5922c21c33527ec
tianhongce <1196585084@qq.com>
2016-12-08 00:02:19 +0800

    views12080002

--- views/src/app/app.module.ts
+++ views/src/app/app.module.ts
@@ -7,13 +7,17 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { StaffComponent } from './staff/staff.component';
 import { StaffInfoComponent } from './staff/staff-info/staff-info.component';
+import { AdminComponent } from './admin/admin.component';
+import { AdminInfoComponent } from './admin/admin-info/admin-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     StaffComponent,
-    StaffInfoComponent
+    StaffInfoComponent,
+    AdminComponent,
+    AdminInfoComponent
   ],
   imports: [
     BrowserModule,
